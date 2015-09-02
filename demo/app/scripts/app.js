c454e76157624ace1156f4b14c75a221a6cf7efd
Julian Godesa <julian.godesa@googlemail.com>
2015-09-02 08:38:25 +0200

    added angular demo structure

---
+++ demo/app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name demoApp
+ * @description
+ * # demoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('demoApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
