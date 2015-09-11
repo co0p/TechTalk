13b7ad4c567c5b5f79afc8c5edb3903b5606f137
Julian Godesa <julian.godesa@smava.de>
2015-09-11 18:57:48 +0200

    adapted input form

--- demo/app/scripts/directives/dmScoreForm.js
+++ demo/app/scripts/directives/dmScoreForm.js
@@ -1,7 +1,18 @@
 angular.module('demoApp').directive('dmScoreForm', dmScoreForm);
 
-function dmScoreForm() {
+function dmScoreForm(ScoreService) {
   return {
-    templateUrl: 'views/dmScoreForm.html'
+    templateUrl: 'views/dmScoreForm.html',
+    link: function(scope, element, attrs) {
+      scope.teamA = {};
+      scope.teamB = {};
+
+      scope.onSubmit = onSubmit;
+
+      function onSubmit(teamA, teamB) {
+        ScoreService.add(teamA);
+        ScoreService.add(teamB);
+      }
+    }
   };
 }
