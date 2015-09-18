867607e9a352d80b5438d500fadf7fa8021b180e
Julian Godesa <julian.godesa@googlemail.com>
2015-09-18 08:33:37 +0200

    more coding, more examples in presentation

--- demo/app/scripts/directives/dmScoreForm.js
+++ demo/app/scripts/directives/dmScoreForm.js
@@ -1,7 +1,34 @@
 angular.module('demoApp').directive('dmScoreForm', dmScoreForm);
 
-function dmScoreForm() {
+function dmScoreForm(_, ScoreService) {
   return {
-    templateUrl: 'views/dmScoreForm.html'
+    templateUrl: 'views/dmScoreForm.html',
+    link: function(scope, element, attrs) {
+
+      // create data structure in template
+      scope.score = {
+        players: {},
+        goals: {}
+      };
+
+      // forward functionality to template
+      scope.isValidToSubmit = isValidToSubmit;
+      scope.submit = submit;
+
+      // return true if all necessary fields are present, otherwise false
+      function isValidToSubmit(score) {
+        return !(_.isEmpty(score.players.a1) || _.isEmpty(score.players.b1)) &&
+        _.isNumber(score.goals.a) && _.isNumber(score.goals.b);
+
+      }
+
+      // submit score, on success reset the form
+      function submit(score) {
+        ScoreService.add(score).then(function() {
+          scope.score.players = {};
+          scope.score.goals = {};
+        });
+      }
+    }
   };
 }
